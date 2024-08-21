99ae2ae722ad57377da3b1102f3519eaeb6fa967
Yannick Charron <ycharron@cstj.qc.ca>
2024-08-21 16:04:18 -0400

    feature: révision variable

---
+++ S01.JS/variables.js
@@ -0,0 +1,22 @@
+
+const prenom = "Yannick"; // Impossible de refaire =
+
+let age = 35; // Définir une variable qui peut être réaffectée, je peux refaire un = (1%)
+
+age += 1;
+age++;
+
+console.log(prenom);
+
+function addition(nombre1, nombre2) {
+    return nombre1 + nombre2;
+}
+
+const resultat = addition(4,6);
+console.log(resultat);
+
+function creerMessage(nom, age) {
+    return `Mon nom est ${nom} et j'ai ${age} ans`;
+}
+
+console.log(creerMessage(prenom, age));
